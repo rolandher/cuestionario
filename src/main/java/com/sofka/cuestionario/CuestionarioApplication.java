3bee361d238a9cf8808d1fda61806172f3bcca1a
Rolando Hernandez <rolandoantoniohe@ufps.edu.co>
2022-04-26 10:38:02 -0500

    Este es el primer archivo

---
+++ src/main/java/com/sofka/cuestionario/CuestionarioApplication.java
@@ -0,0 +1,17 @@
+package com.sofka.cuestionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuestionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuestionarioApplication.class, args);
+		System.out.println("Hola");
+	}
+
+}
+
+
+
---
+++ src/test/java/com/sofka/cuestionario/CuestionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.cuestionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuestionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
