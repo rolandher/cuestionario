dfbc739233014a9d39b7dca4cb988e963304e6c0
rolandher <rolandoantoniohe@ufps.edu.co>
2022-04-26 10:54:46 -0500

    Modificaciones en el main

--- src/main/java/com/sofka/cuestionario/CuestionarioApplication.java
+++ src/main/java/com/sofka/cuestionario/CuestionarioApplication.java
@@ -3,12 +3,21 @@ package com.sofka.cuestionario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class CuestionarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CuestionarioApplication.class, args);
-		System.out.println("Hola");
+		Scanner scanner = new Scanner(System.in);
+		String user;
+		System.out.println("*****************************");
+		System.out.println("*****cuestionario******");
+		System.out.println("*******************************");
+		System.out.println("Por favor ingrese su nombre: ");
+		user = scanner.nextLine();
+		System.out.println("Bienvenido"+ user);
 	}
 
 }
